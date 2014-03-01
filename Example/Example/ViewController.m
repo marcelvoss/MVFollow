1ce4f70aa38ab5c75e36ef1ba57d84a33d119d3c
Marcel Voss <hello@marcelvoss.com>
2014-03-01 13:44:04 +0100

    Modified the UI of the example project and also added two IBActions and one IBOutlet.

--- Example/Example/ViewController.m
+++ Example/Example/ViewController.m
@@ -8,6 +8,8 @@
 
 #import "ViewController.h"
 
+#import "MVFollow.h"
+
 @interface ViewController ()
 
 @end
@@ -18,12 +20,25 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    self.usernameTextField.placeholder = @"@CocoaMarcel";
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)followButton:(id)sender
+{
+    [MVFollow followUser:self.usernameTextField.text];
+}
+
+- (IBAction)openProfileButton:(id)sender
+{
+    // Replace TwitterClientOfficial with the Twitter client of your choice
+    [MVFollow openProfile:self.usernameTextField.text inClient:TwitterClientWeb];
 }
 
 @end
