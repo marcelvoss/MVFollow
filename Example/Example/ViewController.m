1f319291eb947604aef9507a5feccc533a02a0df
Marcel Voss <hello@marcelvoss.com>
2014-03-01 14:27:51 +0100

    …

--- Example/Example/ViewController.m
+++ Example/Example/ViewController.m
@@ -21,7 +21,7 @@
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
     
-    self.usernameTextField.placeholder = @"@CocoaMarcel";
+    self.usernameTextField.placeholder = @"CocoaMarcel";
 }
 
 - (void)didReceiveMemoryWarning
