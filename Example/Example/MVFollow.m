a2ea7d7a9fbd47efd95958d10032d98024650f2c
Marcel Voss <hello@marcelvoss.com>
2014-03-01 18:32:53 +0100

    Following people works now but I still have to implement error handling and support to follow people when the user has more than one account stored.

--- Example/Example/MVFollow.m
+++ Example/Example/MVFollow.m
@@ -8,11 +8,48 @@
 
 #import "MVFollow.h"
 
+#define TWITTER_API_URL @"https://api.twitter.com/1.1/friendships/create.json"
+
 @implementation MVFollow
 
 + (void)followUser:(NSString *)username
 {
-    
+    ACAccountStore *accountStore = [ACAccountStore new];
+    ACAccountType *accountType = [accountStore accountTypeWithAccountTypeIdentifier:ACAccountTypeIdentifierTwitter];
+    [accountStore requestAccessToAccountsWithType:accountType options:nil completion:^(BOOL granted, NSError *error)
+    {
+        if (granted)
+        {
+            NSArray *accounts = [accountStore accountsWithAccountType:accountType];
+            if ([accounts count] > 0)
+            {
+                // Only one account is available
+                NSMutableDictionary *dictionary = [NSMutableDictionary new];
+                [dictionary setObject:@"true" forKey:@"follow"];
+                [dictionary setObject:username forKey:@"screen_name"];
+                
+                SLRequest *request = [SLRequest requestForServiceType:SLServiceTypeTwitter requestMethod:SLRequestMethodPOST URL:[NSURL URLWithString:TWITTER_API_URL] parameters:dictionary];
+                [request setAccount:[accounts objectAtIndex:0]];
+                
+                [request performRequestWithHandler:^(NSData *responseData, NSHTTPURLResponse *urlResponse, NSError *error)
+                {
+                    if (error == nil)
+                    {
+                        NSLog(@"Followed: %@", username);
+                    }
+                    else
+                    {
+                        NSLog(@"The following error occured: %@", error);
+                    }
+                }];
+            }
+            else
+            {
+                // More than one accounts are available
+                
+            }
+        }
+    }];
 }
 
 + (void)openProfile:(NSString *)username inClient:(TwitterClient)twitterClient
