e40304495dd06a45e1f64404f9ba32d977e4c52c
Marcel Voß <me@marcelvoss.com>
2016-12-13 14:22:08 +0100

    Add Swift file for re-write

---
+++ Example/Example/Follow.swift
@@ -0,0 +1,13 @@
+//
+//  Follow.swift
+//  Example
+//
+//  Created by Marcel Voß on 09/12/2016.
+//  Copyright © 2016 Marcel Voß. All rights reserved.
+//
+
+import UIKit
+
+class Follow: NSObject {
+
+}
